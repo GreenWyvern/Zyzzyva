b76f6aaa08c9f69ee6a3779df916c7c1c4ea2927
Guest17432 <dalmacio347@gmail.com>
2018-01-05 15:53:31 -0500

    Camera
    Camera now focuses on player as it moves

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player; 
+
+	private Vector3 focus; 
+
+	void Start () 
+	{
+		focus = transform.position - player.transform.position;
+	}
+
+
+	void LateUpdate () 
+	{
+
+		transform.position = player.transform.position + focus;
+	}
+}
